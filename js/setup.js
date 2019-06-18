a2d075e1c31839a44033565be57c9e0a873d647b
Aznaur <aznaur.khapchaev@gmail.com>
2019-06-18 17:43:37 +0300

    Правки travis

--- js/setup.js
+++ js/setup.js
@@ -2,20 +2,20 @@
 var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var lastNames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var coatColors = [
-    'rgb(101, 137, 164)', 
-    'rgb(241, 43, 107)', 
-    'rgb(146, 100, 161)', 
-    'rgb(56, 159, 117)', 
-    'rgb(215, 210, 55)',
-    'rgb(0, 0, 0)'
+  'rgb(101, 137, 164)',
+  'rgb(241, 43, 107)',
+  'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)'
 ];
 
 var eyeColors = [
-    'black',
-    'red',
-    'blue',
-    'yellow',
-    'green'
+  'black',
+  'red',
+  'blue',
+  'yellow',
+  'green'
 ];
 
 
@@ -29,54 +29,47 @@ var similarWizardTemplate = document.querySelector('#similar-wizard-template')
     .querySelector('.setup-similar-item');
 
 var heroes = [
-    {
-        name: names[randomInteger(0,7)] + " " + lastNames[randomInteger(0,7)],
-        coatColor: coatColors[randomInteger(0, coatColors.length - 1)],
-        eyesColor: eyeColors[randomInteger(0, eyeColors.length - 1)]
-    },
-    {
-        name: names[randomInteger(0,7)] + " " + lastNames[randomInteger(0,7)],
-        coatColor: coatColors[randomInteger(0, coatColors.length - 1)],
-        eyesColor: eyeColors[randomInteger(0, eyeColors.length - 1)]
-    },
-    {
-        name: names[randomInteger(0,7)] + " " + lastNames[randomInteger(0,7)],
-        coatColor: coatColors[randomInteger(0, coatColors.length - 1)],
-        eyesColor: eyeColors[randomInteger(0, eyeColors.length - 1)]
-    },
-    {
-        name: names[randomInteger(0,7)] + " " + lastNames[randomInteger(0,7)],
-        coatColor: coatColors[randomInteger(0, coatColors.length - 1)],
-        eyesColor: eyeColors[randomInteger(0, eyeColors.length - 1)]
-    }
-]
+  {
+    name: names[randomInteger(0,7)] + " " + lastNames[randomInteger(0,7)],
+    coatColor: coatColors[randomInteger(0, coatColors.length - 1)],
+    eyesColor: eyeColors[randomInteger(0, eyeColors.length - 1)]
+  },
+  {
+    name: names[randomInteger(0,7)] + " " + lastNames[randomInteger(0,7)],
+    coatColor: coatColors[randomInteger(0, coatColors.length - 1)],
+    eyesColor: eyeColors[randomInteger(0, eyeColors.length - 1)]
+  },
+  {
+    name: names[randomInteger(0,7)] + " " + lastNames[randomInteger(0,7)],
+    coatColor: coatColors[randomInteger(0, coatColors.length - 1)],
+    eyesColor: eyeColors[randomInteger(0, eyeColors.length - 1)]
+  },
+  {
+    name: names[randomInteger(0,7)] + " " + lastNames[randomInteger(0,7)],
+    coatColor: coatColors[randomInteger(0, coatColors.length - 1)],
+    eyesColor: eyeColors[randomInteger(0, eyeColors.length - 1)]
+  }
+];
 
 
 var renderWizard = function (wizard) {
-    var wizardElement = similarWizardTemplate.cloneNode(true);
-    
-    wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
-    wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
-    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
-    return wizardElement;
-  }
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+  return wizardElement;
+};
 
 var fragment = document.createDocumentFragment();
 for (var i = 0; i < heroes.length; i++) {
-    fragment.appendChild(renderWizard(heroes[i])); 
+  fragment.appendChild(renderWizard(heroes[i])); 
 }
 similarListElement.appendChild(fragment);
 
 userDialog.querySelector('.setup-similar').classList.remove('hidden');
 
 function randomInteger(min, max) {
-  var rand = min - 0.5 + Math.random() * (max - min + 1)
+  var rand = min - 0.5 + Math.random() * (max - min + 1);
   rand = Math.round(rand);
   return rand;
 }
-
-
-
-
-
-
